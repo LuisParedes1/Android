64075e7247881a546b027b058438a936c2d946e5
Luis Paredes <lparedesr@fi.uba.ar>
2023-12-19 14:39:49 -0300

    Added variable lesson

---
+++ kotlin/variables/README.md
@@ -0,0 +1,12 @@
+# Basic data types
+
+![dataType](image.png)
+
+# Type inference
+
+Type inference is when the Kotlin compiler can infer (or determine) what data type a variable should be, without the type being explicitly written in the code.
+
+val edad = 2 // edad takes the type of initial value
+val edad:Int = 2
+
+This expressions are equavalent to the compiler
--- kotlin/variables/var.kt
+++ kotlin/variables/var.kt
@@ -1,6 +1,16 @@
 fun main(){
-    var name:String = "Luis" // variable
-    val edad:Int = 24 // value (const)
+    var name:String = "Luis" // var: variable that changes
+    val edad:Int = 24 // val: value (defines a variable that changes)
 
-    println("hello " + name + " tienes: $edad años")
+    val varInCamelCase: String = "always"
+
+    println("hello " + name + " you are: $edad years old")
+
+    // Type inference
+    // The compiler can infer variable tipe
+
+    val edad1 = 2
+    val edad2:Int = 2
+
+    println("edad1 is the same as edad2: ${edad1 == edad2}")
 }
