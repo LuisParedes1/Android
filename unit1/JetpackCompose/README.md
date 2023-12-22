67d78a404e254672aa577de4e7beb4780e252d5b
Luis Paredes <lparedesr@fi.uba.ar>
2023-12-22 12:42:59 -0300

    Birthday Card App

--- unit1/JetpackCompose/README.md
+++ unit1/JetpackCompose/README.md
@@ -4,4 +4,7 @@ Building a birthday card app using Jetpack Compose
 
 # Notion
 
-More info [here](https://mis-notas.notion.site/Unit-1-3-Build-a-basic-layout-2ec94ab678304e15b670ca1787e06238?pvs=4)
+More info
+
+[Part 1 - Adding Text](https://mis-notas.notion.site/Unit-1-3-3-03cf470d78154d4293440e2a7e8330e2?pvs=4)
+[Parte 2 - Adding Images](https://mis-notas.notion.site/Unit-1-3-4-b4a0ade5e53640f8a467f5b1592472ca?pvs=4)
--- unit1/JetpackCompose/app/src/main/java/com/example/birthdaycard/MainActivity.kt
+++ unit1/JetpackCompose/app/src/main/java/com/example/birthdaycard/MainActivity.kt
@@ -3,9 +3,10 @@ package com.example.birthdaycard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -14,6 +15,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,10 +32,12 @@ class MainActivity : ComponentActivity() {
             BirthdayCardTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingText(messsage = "Hello", from = "From Luis")
+                    GreetingImage(
+                        message = stringResource(R.string.happy_birthday_text),
+                        from = stringResource(R.string.signature_text)
+                    )
                 }
             }
         }
@@ -38,13 +45,16 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingText(messsage: String, from: String = "From Luis", modifier: Modifier = Modifier) {
+fun GreetingText(message: String, from: String = "From Luis", modifier: Modifier = Modifier) {
 
     // Recollect that you need to pass the modifier parameter to the child element in the composables.
     // That means you need to pass the modifier parameter to the Column composable.
-    Column (modifier = modifier.padding(8.dp),
-        verticalArrangement = Arrangement.Center) {
-        Text(text = messsage,
+    Column (
+        modifier = modifier.padding(8.dp),
+        verticalArrangement = Arrangement.Center
+        )
+    {
+        Text(text = message,
             fontSize = 100.sp,
             lineHeight = 116.sp,
             textAlign = TextAlign.Center
@@ -53,16 +63,52 @@ fun GreetingText(messsage: String, from: String = "From Luis", modifier: Modifie
             fontSize = 36.sp,
             modifier = Modifier
                 .padding(16.dp)
-                .align(alignment = Alignment.End)
-            )
+                .align(alignment = Alignment.CenterHorizontally)
+        )
     }
 
+}
+
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier){
+
+    // The painterResource() function loads a drawable image resource,
+    val image:Painter = painterResource(R.drawable.androidparty)
+
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F
+        )
+
+        GreetingText(
+            message = message,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true, name = "Birthday")
 @Composable
 fun BirthdayCardPreview() {
     BirthdayCardTheme {
-        GreetingText(messsage = "Happy Birthday Jose!", from = "From Luis")
+        GreetingText(
+            message = stringResource(R.string.happy_birthday_text),
+            from = stringResource(R.string.signature_text)
+        )
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingCardPreview(){
+    GreetingImage(
+        message = stringResource(R.string.happy_birthday_text),
+        from = stringResource(R.string.signature_text)
+    )
 }
