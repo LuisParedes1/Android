687cc52194e62b23c419e71ea4958d5d9dfa5c94
Luis Paredes <lparedesr@fi.uba.ar>
2023-12-22 18:53:46 -0300

    Exercise: Task Manager

--- unit1/Exercises/taskManager/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ unit1/Exercises/taskManager/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,18 +3,35 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,7 +39,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val finishedText:String = stringResource(R.string.finished_text)
+                    val goodJobText:String = stringResource(R.string.nice_work_text)
+                    val finishedImg: Painter = painterResource(R.drawable.ic_task_completed)
+
+                    TaskCompletedScreen(finishedText, goodJobText, finishedImg)
                 }
             }
         }
@@ -30,17 +51,40 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TaskCompletedScreen(finished: String, goodJob: String, finishedImg: Painter, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        Image(
+            painter = finishedImg,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+        )
+        Text(
+            text = finished,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(top = 24.dp, bottom = 8.dp)
+        )
+        Text(
+            text = goodJob,
+            fontSize = 16.sp
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyApplicationTheme {
-        Greeting("Android")
+        TaskCompletedScreen(
+            stringResource(R.string.finished_text),
+            stringResource(R.string.nice_work_text),
+            painterResource(R.drawable.ic_task_completed)
+        )
     }
 }
