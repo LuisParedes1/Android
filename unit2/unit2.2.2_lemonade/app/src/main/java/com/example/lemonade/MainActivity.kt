46ad1a0e3f6d774e5a90144f5aa7d0393c504ae5
Luis Paredes <lparedesr@fi.uba.ar>
2024-01-16 12:16:31 -0300

    Refactor

--- unit2/unit2.2.2_lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
+++ unit2/unit2.2.2_lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -5,24 +5,20 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -49,24 +45,24 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun ImgAndTextScreen(img: Painter,
-                     imgDesc: String,
-                     msgText: String ,
-                     onClick: ()->Unit,
-                     modifier: Modifier = Modifier){
+fun LemonImgAndTextScreen(img: Painter,
+                          imgDesc: String,
+                          msgText: String,
+                          onClick: ()->Unit,
+                          modifier: Modifier = Modifier){
     Column (
         horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
         modifier = modifier
     ){
 
-        Button(
-            onClick = onClick,
-            ) {
-            Image(img,
-                contentDescription = imgDesc,
-                modifier = modifier.background(Color.Green)
-            )
-        }
+
+        Image(img,
+            contentDescription = imgDesc,
+            modifier = modifier
+                .background(Color.Green)
+                .clickable(enabled = true, onClick = onClick)
+        )
 
         Spacer(modifier =  Modifier.height(16.dp))
 
@@ -133,12 +129,16 @@ fun LemonadeApp(modifier: Modifier = Modifier) {
         }
     }
 
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+
     Box (
         modifier = modifier
-            .fillMaxSize()
             .wrapContentSize(Alignment.Center)
     ){
-        ImgAndTextScreen(
+        LemonImgAndTextScreen(
             img,
             imgDescription,
             msg,
@@ -146,6 +146,6 @@ fun LemonadeApp(modifier: Modifier = Modifier) {
             modifier
         )
     }
-
+    }
 }
 
